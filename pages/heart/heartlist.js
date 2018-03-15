516de837d913191ac1b12e0d1074af4b2635d9f8
数海信息 <shuhaixinxi@shuhaixinxideMac-mini.local>
2018-03-15 18:33:01 +0800

    实现点击拥抱 调用接口

--- pages/heart/heartlist.js
+++ pages/heart/heartlist.js
@@ -32,9 +32,14 @@ Page({
 
     console.log("-----scroll to load ");
   },
+  refresh:function(){
+
+  },
   pullUpLoad:function () {
     
   },
+  scrollV:function(){},
+
   onLoad: function () {
     that = this;
     wx.startPullDownRefresh();
@@ -43,6 +48,37 @@ Page({
   onShow: function () {
     getList();
   },
+
+  clickGood:function(e){
+    var good = e.currentTarget.dataset.good;
+    var id = e.currentTarget.dataset.id;
+    if(good==undefined){
+      good = 1;
+    }else{
+      good+=1;
+    }
+
+    var Epigram = Bmob.Object.extend("epigram");
+    var epigram = new Epigram();
+    // 这个 id 是要修改条目的 objectId，你在
+    //这个存储并成功时可以获取到，请看前面的文档
+    epigram.get(id, {
+      success: function (result) {
+        // 回调中可以取得这个 GameScore 对象的一个实例，然后就可以修改它了
+        result.set('good', good);
+        result.save();
+        // The object was retrieved successfully.
+      },
+      error: function (object, error) {
+        console.log('save error '+error);
+      }
+    });
+
+    console.log("clickGood--" + id+" good:"+good);
+  },
+  clickHug:function(e){
+    console.log("clickHug----<><>" + e.currentTarget.dataset.hug);
+  }
 
 })
 
