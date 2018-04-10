869c47c41708844016a7f04cd5f0a0f7a72266d4
数海信息 <shuhaixinxi@shuhaixinxideMac-mini.local>
2018-04-10 17:48:58 +0800

    新列表展示头像，名称

--- pages/heart/heartlist.js
+++ pages/heart/heartlist.js
@@ -1,27 +1,31 @@
 //logs.js
 //const util = require('../../utils/util.js')
 var Bmob = require('../../utils/bmob.js');
+var app = getApp();
+const appParam = app.appParam;
 var that;
 
 Page({
   data: {
-    epigramList:[],
+    epigramList: [],
     toView: 'red',
-    scrollTop:100,
-    loadingHidden:true,
-    scrollLog:'0',
+    scrollTop: 100,
+    loadingHidden: true,
+    scrollLog: '0',
     limit: 10,
-    logevent:"world"
+    logevent: "world",
+    userInfo: {},
+    hasUserInfo: false
   },
 
   //下拉刷新
-  onPullDownRefresh:function(){
+  onPullDownRefresh: function () {
     console.log("onPullDownRefresh");
     getList();
   },
   onReachBottom: function () {
     this.setData({
-      loadingHidden:false
+      loadingHidden: false
     })
 
     var limit = that.data.limit + 10
@@ -32,13 +36,13 @@ Page({
 
     console.log("-----scroll to load ");
   },
-  refresh:function(){
+  refresh: function () {
 
   },
-  pullUpLoad:function () {
-    
+  pullUpLoad: function () {
+
   },
-  scrollV:function(){},
+  scrollV: function () { },
 
   onLoad: function () {
     that = this;
@@ -49,35 +53,93 @@ Page({
     getList();
   },
 
-  clickGood:function(e){
-    var good = e.currentTarget.dataset.good;
+  clickGood: function (e) {
+    let good = e.currentTarget.dataset.good;  //点赞次数
     var id = e.currentTarget.dataset.id;
-    if(good==undefined){
+
+    let epigrams = that.data.epigramList;
+    console.log("epigrams size is " + epigrams.length);
+
+    // epigrams.forEach(function (item, index) {
+    //   if (item.objectId == id) {
+    //     console.log("foreach id = " + id);
+    //     item.hgood = !item.hgood;
+    //   }
+    // });
+
+    epigrams.forEach((item) => {
+      if (item.id == id) {
+        console.log("item object id " + item.id + "  " + item.attributes.say);
+        item.hgood = !item.hgood || false;
+      }
+    })
+
+    that.setData({
+      epigramList: that.data.epigramList
+    });
+
+    let currentIndex = that.data.epigramList.findIndex(e => e.id === id);
+    // console.log("click good id is " + id + " index:" + currentIndex);
+
+    var epi = that.data.epigramList[currentIndex];
+
+    // console.log("click good status " + that.data.epigramList.find((e) => e.objectId === id).say);
+
+    // that.setData({
+    //   [that.data.epigramList[currentIndex].hgood]: true
+    // })
+
+    //arr-对象|index-索引
+    let tmp = that.data.epigramList.map(function (arr, index) {
+      if (arr.objectId == id) {
+        console.log("遇到对的人：" + id);
+      }
+      return arr;
+    });
+
+    if (good == undefined) {
       good = 1;
-    }else{
-      good+=1;
+    } else {
+      good += 1;
     }
 
     var Epigram = Bmob.Object.extend("epigram");
     var epigram = new Epigram();
-    // 这个 id 是要修改条目的 objectId，你在
+    //这个 id 是要修改条目的 objectId，你在
     //这个存储并成功时可以获取到，请看前面的文档
     epigram.get(id, {
       success: function (result) {
+        console.log("get success");
         // 回调中可以取得这个 GameScore 对象的一个实例，然后就可以修改它了
         result.set('good', good);
         result.save();
+
         // The object was retrieved successfully.
       },
       error: function (object, error) {
-        console.log('save error '+error);
+        console.log('save error ' + error);
       }
     });
 
-    console.log("clickGood--" + id+" good:"+good);
   },
-  clickHug:function(e){
+  clickHug: function (e) {
     console.log("clickHug----<><>" + e.currentTarget.dataset.hug);
+  },
+  createBtnClick:function(e){
+
+    console.log("createBtnClick");
+    wx.navigateTo({
+      url: '../push/push',
+      success:function(){
+        console.log("success");
+      },
+      fail:function(result){
+        console.log("fail"+result);
+      },
+      complete:function(){
+        console.log("complete");
+      }
+    })
   }
 
 })
@@ -92,7 +154,6 @@ function getList() {
     success: function (results) {
       wx.stopPullDownRefresh();
       // 循环处理查询到的数据
-      console.log(results);
       that.setData({
         loadingHidden: true,
         epigramList: results
--- pages/push/push.js
+++ pages/push/push.js
@@ -1,13 +1,17 @@
 var Bmob = require('../../utils/bmob.js');
+const app = getApp();
+const appParam = app.appParam;
+var that;
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     epigramVal:"",
     btnVal: "1",
+    userInfo: {},
+    openId:"",
     broadColor:"#6e7d8f",
     broadCols: ["#3c3d3c", "#f5d575", "#f69595", "#c5a6c1", "#71aaa9", "#90cbe3", 
     "#6e7d8f", "#62ad98", "#d3cc72", "#e9d4a9", "#dba6a1"]
@@ -22,23 +26,38 @@ Page({
   },
 
   contentInput:function(e){
+    console.log("32424："+e);
+
     this.setData({
       epigramVal: e.detail.value
     })
   },
 
   formSubmit:function(e){
+    getUserOpenId();
+    var val = this.data.epigramVal;
+    if(val==""){
+      wx.showToast({
+        title: '请输入想说的话！',
+        image: '../image/warning.png',
+        duration: 1000
+      });
+      console.log('val is empty');
+      return;
+    }
     var Epigram = Bmob.Object.extend("epigram");
     var epigram = new Epigram();
-    epigram.set("userId",);
-    epigram.set("say", this.data.epigramVal);
+    epigram.set("userId",this.data.openId);
+    epigram.set("say", val);
+    epigram.set("nickName", this.data.userInfo.nickName);
+    epigram.set("avatarUrl", this.data.userInfo.avatarUrl);
     epigram.set("color", this.data.broadColor);
     //添加数据，第一个入口参数是null
     epigram.save(null, {
       success: function (result) {
         wx.showToast({
           title: '发布成功！',
-          icon: 'success',
+          image: '../image/success.png',
           duration: 1000
         });
         // 添加成功，返回成功之后的objectId（注意：返回的属性名字是id，不是objectId），你还可以在Bmob的Web管理后台看到对应的数据
@@ -48,7 +67,7 @@ Page({
         // 添加失败
         wx.showToast({
           title: '发布失败！',
-          icon: 'success',
+          image: '../image/warning.png',
           duration: 1000
         });
         console.log('创建日记失败' + error);
@@ -60,55 +79,72 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    that = this;
+    getUserOpenId();
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
+  
 })
+
+function getUserOpenId() {
+  wx.login({
+    success: function (code) {
+      wx.request({
+        url: 'https://api.weixin.qq.com/sns/jscode2session',
+        data: {
+          appid: 'wxb4ddb059c36f48c3',
+          secret: 'cf6d03f9d3e77b43ec9d5f2c34e090ed',
+          js_code: code.code,
+          grant_type: 'authorization_code'
+        },
+        success: function (openIdResult) {
+          let openid = openIdResult.data.openid;
+          that.setData({
+            openId:openid
+          });
+          console.log('openId:' + openid);
+          // wx.setStorage({
+          //   key: "user_openid",
+          //   data: openIdResult.data.openid,
+          //   success: function () {
+          //   },
+          // });
+        }
+      })
+    }
+  });
+};
