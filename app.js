92ee2fb772b7d28934bbff958f1fe0563e259b20
数海信息 <shuhaixinxi@shuhaixinxideMac-mini.local>
2018-04-11 18:01:56 +0800

    我的页面添加滑动控件

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 //app.js
+var that;
+
 App({
   onLaunch: function () {
+
+    that = this;
 
     const Bmob = require('utils/bmob.js');
     Bmob.initialize("dce0be3d571021a9eac5f0a321a79881", "62a2cb5b8baf5b9bc43d8c43bd9a803c");
 
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log('app wx login success');
+        getUserOpenId();
       }
     })
     // 获取用户信息
@@ -36,8 +42,33 @@ App({
         }
       }
     })
+
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    openId:""
   }
+
 })
+
+function getUserOpenId() {
+  console.log("App getUserOpenId");
+  wx.login({
+    success: function (code) {
+      wx.request({
+        url: 'https://api.weixin.qq.com/sns/jscode2session',
+        data: {
+          appid: 'wxb4ddb059c36f48c3',
+          secret: 'cf6d03f9d3e77b43ec9d5f2c34e090ed',
+          js_code: code.code,
+          grant_type: 'authorization_code'
+        },
+        success: function (openIdResult) {
+          let openid = openIdResult.data.openid;
+          that.globalData.openId = openid;
+          console.log('app openId:' + openid);
+        }
+      })
+    }
+  });
+};
--- pages/mine/me.js
+++ pages/mine/me.js
@@ -1,11 +1,25 @@
 //index.js
 //获取应用实例
+var Bmob = require('../../utils/bmob.js');
 const app = getApp()
+var that;
 
 Page({
   data: {
+    tabs: ['我发布的', '我收藏的'],
+    stv: {
+      windowWidth: 0,
+      lineWidth: 0,
+      offset: 0,
+      tStart: false
+    },
+    activeTab: 0,
     userInfo: {},
+    epigramList: [],
     hasUserInfo: false,
+    loadingHidden: true,
+    scrollLog: '0',
+    limit: 10,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
@@ -14,7 +28,49 @@ Page({
       url: '../logs/logs'
     })
   },
+
+  //下拉刷新
+  onPullDownRefresh: function () {
+    console.log("onPullDownRefresh");
+    getList();
+  },
+  onReachBottom: function () {
+    this.setData({
+      loadingHidden: false
+    })
+
+    var limit = that.data.limit + 10
+    this.setData({
+      limit: limit
+    })
+    this.onShow();
+
+    console.log("-----scroll to load ");
+  },
+  refresh: function () {
+
+  },
+  pullUpLoad: function () {
+
+  },
+  scrollV: function () { },
+
+  onShow: function () {
+    getList();
+  },
+
   onLoad: function () {
+    try {
+      let { tabs } = this.data;
+      var res = wx.getSystemInfoSync()
+      this.windowWidth = res.windowWidth;
+      this.data.stv.lineWidth = this.windowWidth / this.data.tabs.length;
+      this.data.stv.windowWidth = res.windowWidth;
+      this.setData({ stv: this.data.stv })
+      this.tabsCount = tabs.length;
+    } catch (e) {
+    }
+
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -41,6 +97,11 @@ Page({
         }
       })
     }
+
+    that = this;
+    wx.startPullDownRefresh();
+    getList();
+
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -49,5 +110,108 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  handlerStart(e) {
+    let { clientX, clientY } = e.touches[0];
+    this.startX = clientX;
+    this.tapStartX = clientX;
+    this.tapStartY = clientY;
+    this.data.stv.tStart = true;
+    this.tapStartTime = e.timeStamp;
+    this.setData({ stv: this.data.stv })
+  },
+  handlerMove(e) {
+    let { clientX, clientY } = e.touches[0];
+    let { stv } = this.data;
+    let offsetX = this.startX - clientX;
+    this.startX = clientX;
+    stv.offset += offsetX;
+    if (stv.offset <= 0) {
+      stv.offset = 0;
+    } else if (stv.offset >= stv.windowWidth * (this.tabsCount - 1)) {
+      stv.offset = stv.windowWidth * (this.tabsCount - 1);
+    }
+    this.setData({ stv: stv });
+  },
+  handlerCancel(e) {
+
+  },
+  handlerEnd(e) {
+    let { clientX, clientY } = e.changedTouches[0];
+    let endTime = e.timeStamp;
+    let { tabs, stv, activeTab } = this.data;
+    let { offset, windowWidth } = stv;
+    //快速滑动
+    if (endTime - this.tapStartTime <= 300) {
+      //向左
+      if (Math.abs(this.tapStartY - clientY) < 50) {
+        if (this.tapStartX - clientX > 5) {
+          if (activeTab < this.tabsCount - 1) {
+            this.setData({ activeTab: ++activeTab })
+          }
+        } else {
+          if (activeTab > 0) {
+            this.setData({ activeTab: --activeTab })
+          }
+        }
+        stv.offset = stv.windowWidth * activeTab;
+      } else {
+        //快速滑动 但是Y距离大于50 所以用户是左右滚动
+        let page = Math.round(offset / windowWidth);
+        if (activeTab != page) {
+          this.setData({ activeTab: page })
+        }
+        stv.offset = stv.windowWidth * page;
+      }
+    } else {
+      let page = Math.round(offset / windowWidth);
+      if (activeTab != page) {
+        this.setData({ activeTab: page })
+      }
+      stv.offset = stv.windowWidth * page;
+    }
+    stv.tStart = false;
+    this.setData({ stv: this.data.stv })
+  },
+  _updateSelectedPage(page) {
+    let { tabs, stv, activeTab } = this.data;
+    activeTab = page;
+    this.setData({ activeTab: activeTab })
+    stv.offset = stv.windowWidth * activeTab;
+    this.setData({ stv: this.data.stv })
+  },
+  handlerTabTap(e) {
+    this._updateSelectedPage(e.currentTarget.dataset.index);
   }
 })
+
+function getList() {
+
+  var wxId = app.globalData.openId;
+
+  console.log('11 wxId:' + wxId);
+
+  var Epigram = Bmob.Object.extend("epigram");
+  var query = new Bmob.Query(Epigram);
+  // 查询个人发布数据
+  query.limit(that.data.limit);
+  query.equalTo("userId", wxId);
+  query.descending("createdAt");
+  query.find({
+    success: function (results) {
+      wx.stopPullDownRefresh();
+      // 循环处理查询到的数据
+      that.setData({
+        loadingHidden: true,
+        epigramList: results
+      })
+    },
+    error: function (error) {
+      wx.stopPullDownRefresh();
+      that.setData({
+        loadingHidden: true
+      })
+      console.log("查询失败: " + error.code + " " + error.message);
+    }
+  });
+}
